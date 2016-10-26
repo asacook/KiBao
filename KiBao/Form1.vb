b4bae028167eea187280f8662a9fa1f84315d295
Andrew Cook <asacook1@sheffield.ac.uk>
2016-10-26 23:23:40 +0100

    project started

---
+++ KiBao/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim PlayerX(15) As Label
+        Dim PlayerY(15) As Label
+
+
+
+
+    End Sub
+End Class
